5fc4fde87879b31e4243375dcf460a2f8981ce64
DC_RedStone <1516235445@qq.com>
2022-09-15 13:19:29 +0800

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,16 +1,1 @@
-sudo apt-get install git-core gnupg flex bison maven gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386  lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip squashfs-tools python-mako libssl-dev ninja-build lunzip syslinux syslinux-utils gettext genisoimage gettext bc xorriso libncurses5 xmlstarlet build-essential git imagemagick lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libxml2 lzop pngcrush rsync schedtool python-enum34 python3-mako libelf-dev 
-sudo apt-get install -y openjdk-8-jdk
-sudo apt-get install -y libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-dev g++-multilib
-sudo apt-get install -y git flex bison gperf build-essential libncurses5-dev:i386
-sudo apt-get install -y tofrodos python-markdown libxml2-utils xsltproc zlib1g-dev:i386
-sudo apt-get install -y dpkg-dev libsdl1.2-dev libesd0-dev
-sudo apt-get install -y git-core gnupg flex bison gperf build-essential
-sudo apt-get install -y zip curl zlib1g-dev gcc-multilib g++-multilib
-sudo apt-get install -y libc6-dev-i386
-sudo apt-get install -y lib32ncurses5-dev x11proto-core-dev libx11-dev
-sudo apt-get install -y libgl1-mesa-dev libxml2-utils xsltproc unzip m4
-sudo apt-get install -y lib32z1-dev ccache
-sudo apt-get install -y bc python flex bison gperf libsdl-dev libesd0-dev build-essential zip curl
-sudo apt-get install -y libssl-dev liblz4-tool
-sudo apt-get install -y libesd-java libwxgtk3.0-dev squashfs-tools pngcrush schedtool libxml2
-sudo apt-get install -y lzop schedtool lib32readline-dev libswitch-perl libssl-dev
+sudo apt-get install -y git-core gnupg flex bison maven gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386  lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev ccache libgl1-mesa-dev libxml2-utils xsltproc unzip squashfs-tools python-mako libssl-dev ninja-build lunzip syslinux syslinux-utils gettext genisoimage gettext bc xorriso libncurses5 xmlstarlet build-essential git imagemagick lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libxml2 lzop pngcrush rsync schedtool python-enum34 python3-mako libelf-dev openjdk-8-jdk libx11-dev:i386 libreadline6-dev:i386 libgl1-mesa-dev g++-multilib git flex bison gperf build-essential libncurses5-dev:i386 tofrodos python-markdown libxml2-utils xsltproc zlib1g-dev:i386 dpkg-dev libsdl1.2-dev libesd0-dev git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev libgl1-mesa-dev libxml2-utils xsltproc unzip m4 lib32z1-dev ccache bc python flex bison gperf libsdl-dev libesd0-dev build-essential zip curl libssl-dev liblz4-tool libesd-java libwxgtk3.0-dev squashfs-tools pngcrush schedtool libxml2 lzop schedtool lib32readline-dev libswitch-perl libssl-dev
